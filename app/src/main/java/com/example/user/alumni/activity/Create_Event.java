4064b719f0fbf02279f77a0f0181988744f7b0e8
Netshore <nandu934@gmail.com>
2017-05-17 11:19:36 +0530

    17-05 commit

--- app/src/main/java/com/example/user/alumni/activity/Create_Event.java
+++ app/src/main/java/com/example/user/alumni/activity/Create_Event.java
@@ -1,22 +1,56 @@
 package com.example.user.alumni.activity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.user.alumni.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class Create_Event extends AppCompatActivity {
 
-    private TextView textView;
+    private EditText edittext,ed_time;
+    private TextView tv;
+    private int mYear, mMonth, mDay, mHour, mMinute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create__event);
 
-        textView = (TextView) findViewById(R.id.tv);
+        ed_time = (EditText) findViewById(R.id.time_view_edit);
+        edittext = (EditText) findViewById(R.id.event_date);
         Bundle bundle = getIntent().getExtras();
         String s = bundle.getString("DATE");
-        textView.setText(s);
+        edittext.setText(s);
+
+        ed_time.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(Create_Event.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        ed_time.setText( selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
     }
 }
